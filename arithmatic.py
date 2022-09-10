b2b46a2decd052da7c65d41354a96b83d116b7e4
musfiqur172 <musfiqur.rahman172@gmail.com>
2022-09-11 00:19:30 +0600

    first commit

---
+++ arithmatic.py
@@ -0,0 +1,11 @@
+number1 = input("Please type an interger and press enter: ")
+number2 = input("Please type another integer and press enter: ")
+number1 = int(number1)
+number2 = int(number2)
+print("number1 + number2 = ", number1 + number2)
+print("number1 + number2 = ", number1 - number2)
+print("number1 + number2 = ", number1 * number2)
+print("number1 + number2 = ", number1 / number2)
+print("number1 + number2 = ", number1 // number2)
+print("number1 + number2 = ", number1 % number2)
+print("number1 + number2 = ", number1 ** number2)10
