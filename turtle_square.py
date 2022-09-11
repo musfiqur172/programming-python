3faf256367d0f986f2dd6470b43570fc6232f0e8
musfiqur172 <musfiqur.rahman172@gmail.com>
2022-09-11 16:02:32 +0600

    loop programs added

---
+++ turtle_square.py
@@ -0,0 +1,17 @@
+import turtle
+
+turtle.shape("triangle")
+
+turtle.forward(100)
+turtle.left(90)
+
+turtle.forward(100)
+turtle.left(90)
+
+turtle.forward(100)
+turtle.left(90)
+
+turtle.forward(100)
+turtle.left(90)
+
+turtle.exitonclick()
